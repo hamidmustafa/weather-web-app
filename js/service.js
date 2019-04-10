3ed4759ed1d7d4f768eff88db25cf4d5d91dacda
Hamid <hamidmustafa.07@gmail.com>
2019-04-10 11:40:48 -0400

    Update: Some UI operations are updated

--- js/service.js
+++ js/service.js
@@ -1,14 +1,14 @@
 
 const apiKey = "4ad1114b14d40f4b5181415c0fee5e25";
 let searchButton = document.getElementById("search-btn");
-let searchInput = document.getElementById("search-location");
-let cityName = document.getElementById("location");
+let requiredLocation = document.getElementById("search-location");
+let cityName = document.getElementById("city-name");
 let icon = document.getElementById("icon");
 let temperature = document.getElementById("temp");
 let humidity = document.getElementById("humidity-div");
 
 searchButton.addEventListener("click", findWeatherDetails);
-searchInput.addEventListener("keyup", enterPressed);
+requiredLocation.addEventListener("keyup", enterPressed);
 
 function enterPressed(event) {
     if (event.key === "Enter") {
@@ -17,11 +17,12 @@ function enterPressed(event) {
 }
 
 function findWeatherDetails() {
-    if (searchInput.value === "") {
+    if (requiredLocation.value === "") {
+        // Display a message if user does'nt enter location and presses the "Get Weather" button
         alert("Enter location")
     } else {
-        let apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput.value + "&appid="+apiKey;
-        httpRequestAsync(apiURL, responseFromAPICall);
+        let apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + requiredLocation.value + "&appid="+apiKey;
+        httpAsyncRequest(apiURL, responseFromAPICall);
     }
 }
 
@@ -31,17 +32,18 @@ function responseFromAPICall(response) {
     icon.src = "http://openweathermap.org/img/w/" + jsonResponseObject.weather[0].icon + ".png";
     var tempFromResponse = parseInt(jsonResponseObject.main.temp - 273)
     var celsiusToFahrenheit = (tempFromResponse * (9/5)) + 32;
-    temperature.innerHTML = celsiusToFahrenheit + "°F";
+    var formattedValueOfTemp = Math.round(celsiusToFahrenheit);
+    temperature.innerHTML = formattedValueOfTemp + "°F";
     humidity.innerHTML = jsonResponseObject.main.humidity + "%";
 }
 
-function httpRequestAsync(url, callback) {
+function httpAsyncRequest(url, callback) {
     console.log("hello");
     var httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = () => { 
-        if (httpRequest.readyState == 4 && httpRequest.status == 200)
+        if (httpRequest.readyState == 4 && httpRequest.status == 200) // request is finished and response is ready
         callback(httpRequest.responseText);
     }
-    httpRequest.open("GET", url, true); // true for asynchronous 
+    httpRequest.open("GET", url, true); 
     httpRequest.send();
 }
